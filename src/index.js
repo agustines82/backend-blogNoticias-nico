cd7e982264ecba9868d5a431feae563667f31dac
Agustin Sampayo <agustines82@gmail.com>
2023-03-08 12:10:27 -0300

    agrego la conexion a la base de datos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import "./database";
 
-//instancia de express
+//instanciar express y configurar el puerto:
 const app = express();
-
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"), () => {
     console.log("Estoy en el puerto " + app.get("port"));
 });
 
-console.log("hola mundo");
+//midlewares:
+app.use(cors()); //permite conexiones remotas
+app.use(express.json()); //permite interpretar formato json
+app.use(express.urlencoded({ extends: true })); //permite interpretar formato json
+app.use(morgan("dev"));
+
+//rutas
+app.get("/prueba", (req, res) => {
+    res.send("prueba peticion get");
+});
